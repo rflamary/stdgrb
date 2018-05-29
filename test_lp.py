dcc148b0eac06a9b98f7ab2dee57f5715b41a0e0
Rémi Flamary <remi.flamary@gmail.com>
2018-05-29 11:36:10 +0200

    add comparison to scipy

--- test_lp.py
+++ test_lp.py
@@ -9,6 +9,7 @@ Created on Fri May 11 10:08:27 2018
 import numpy as np
 import pylab as pl
 import scipy
+import scipy.optimize
 
 import stdgrb
 import time
@@ -16,21 +17,23 @@ import time
 t_start=time.clock()
 
 def tic():
+    global t_start
     t_start=time.clock()
 
 def toc():
+    global t_start
     t=time.clock()-t_start
     print('Elapsed time: {:1.3f}s'.format(t))
     return t
 
 #%%
 
-n=1000
-d=100
+n=2000
+d=200
 
 np.random.seed(0)
 
-c=np.random.randn(d)
+c=-np.random.rand(d)
 A=np.random.rand(n,d)
 b=np.random.rand(n)
 
@@ -40,20 +43,34 @@ ub=np.ones(d)
 
 #%%
 
+print('Scipy simplex solver')
+tic()
+sol=scipy.optimize.linprog(c,A,b)
+x0=sol.x
+v0=sol.fun
+toc()
+
+print('Scipy interior point solver')
+tic()
+sol=scipy.optimize.linprog(c,A,b,method='interior-point')
+x00=sol.x
+v00=sol.fun
+toc()
+
 print('Default method')
 tic()
-x=stdgrb.lp_solve(c,A,b,lb,ub,logtoconsole=0)
+x1,v1=stdgrb.lp_solve(c,A,b,lb,ub,logtoconsole=0)
 toc()
 
 
 print('Simplex method')
 tic()
-x=stdgrb.lp_solve(c,A,b,lb,ub,1,logtoconsole=0)
+x2,v2=stdgrb.lp_solve(c,A,b,lb,ub,1,logtoconsole=0)
 toc()
 
 print('Interior point method')
 tic()
-x=stdgrb.lp_solve(c,A,b,lb,ub,2,logtoconsole=0,crossover=0)
+x3,v3=stdgrb.lp_solve(c,A,b,lb,ub,2,logtoconsole=0,crossover=0)
 toc()
 #%%
 
